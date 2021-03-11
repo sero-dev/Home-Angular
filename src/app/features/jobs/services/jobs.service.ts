198d43740b151249a3e388a4c3a6092b4b8974be
Sean Rodriguez <sean.rodriguez@outlook.com>
2021-03-10 23:18:20 -0500

    Rename parameter to camel case; fix an issue in getJobs() where dateLastUpdate was being mapped as a string.

--- src/app/features/jobs/services/jobs.service.ts
+++ src/app/features/jobs/services/jobs.service.ts
@@ -1,10 +1,10 @@
-import { Injectable } from '@angular/core';
-import { Job } from '../models/job.model';
-import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
-import { ListResponse } from '../../../core/models/list-response.model';
-import { NewJob } from '../models/newJob.model';
+import {Injectable} from '@angular/core';
+import {Job} from '../models/job.model';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
+import {ListResponse} from '../../../core/models/list-response.model';
+import {NewJob} from '../models/newJob.model';
 
 @Injectable({
   providedIn: 'root'
@@ -14,21 +14,32 @@ export class JobsService {
   constructor(private http: HttpClient) { }
 
   getJobs(): Observable<Job[]> {
-    return this.http.get<ListResponse<Job>>('https://localhost:5001/api/Jobs').pipe(
-      map(value => value.data)
+    return this.http.get<ListResponse<any>>('https://localhost:5001/api/Jobs').pipe(
+      map(value => {
+        return value.data.map<Job>(response => {
+          const job = new Job();
+          job.jobTitle = response.jobTitle;
+          job.employer = response.employer;
+          job.city = response.city;
+          job.state = response.state;
+          job.status = response.status;
+          job.dateLastUpdated = new Date(response.dateLastUpdated);
+          return job;
+        });
+      })
     );
   }
 
-  postJobs(Job: NewJob) {
-    return this.http.post('https://localhost:5001/api/AddJobs', {
-      jobTitle: Job.jobTitle,
-      employer: Job.employer,
-      city: Job.city,
-      state: Job.state,
-      status: Job.status,
-      dateLastUpdated: Job.dateLastUpdated,
-      minSalary: Job.minSalary,
-      maxSalary: Job.maxSalary
+  postJobs(newJob: NewJob): void {
+    this.http.post('https://localhost:5001/api/Jobs', {
+      jobTitle: newJob.jobTitle,
+      employer: newJob.employer,
+      city: newJob.city,
+      state: newJob.state,
+      status: newJob.status,
+      dateLastUpdated: newJob.dateLastUpdated,
+      minSalary: newJob.minSalary,
+      maxSalary: newJob.maxSalary
     }).subscribe(
         () => {
           console.log('Recording completed !');
